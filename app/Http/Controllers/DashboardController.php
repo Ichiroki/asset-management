a7a9381e2e4475fb18e362cf5f369f64cf1efc54
Ichiroki <fahrezirizqiawan12649@gmail.com>
2023-10-31 13:52:27 +0700

    add radio button to switch asset in loans

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Charts\UserChart;
+use App\Models\Asset\Laptop;
 use App\Models\Asset\Vehicle;
 use App\Models\User;
 
@@ -9,13 +9,11 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $vehicle = Vehicle::all();
-
-        $users = new UserChart;
-        $users->labels(['One', 'Two', 'Three', 'Four']);
-        $users->dataset('Jumlah Mobil', 'line', $vehicle);
+        $vehicle = Vehicle::all()->count();
+        $laptop = Laptop::all()->count();
         return view('pages/dashboard', [
-            'users' => $users
+            'vehicle' => $vehicle,
+            'laptop' => $laptop
         ]);
     }
 }
--- resources/views/pages/loans/vehicle/create.blade.php
+++ resources/views/pages/loans/vehicle/create.blade.php
@@ -8,76 +8,37 @@
             @csrf
             <div class="mb-6">
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
+                <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" value="{{ Auth::user()->name }}" autocomplete="off" autofocus required disabled>
                 @error('name')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label for="no_asset" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">No Asset</label>
-                <input type="text" id="no_asset" name="no_asset" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                @error('no_asset')
+                <label for="dept" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
+                <input type="text" id="dept" name="dept" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" value="{{ Auth::user()->department }}" autofocus required disabled>
+                @error('dept')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="mb-6">
-                <label for="date_used" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tanggal Pakai</label>
-                <input type="date" id="date_used" name="date_used" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                @error('date_used')
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="mb-6">
-                <label for="processor" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prosesor</label>
-                <input type="text" id="processor" name="processor" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                @error('processor')
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="mb-6">
-                <label for="ram" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ram</label>
-                <input type="text" id="ram" name="ram" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                @error('ram')
-                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                @enderror
-            </div>
-            <div>
-                <p class="text-center">Storage</p>
-                <div class="flex gap-3">
-                    <div class="mb-6">
-                        <label for="main_storage" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Main</label>
-                        <input type="text" id="main_storage" name="main_storage" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                        @error('main_storage')
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="mb-6">
-                        <label for="extend_storage" class="text-end block mb-2 text-sm font-medium text-gray-900 dark:text-white">Extends</label>
-                        <input type="text" id="extend_storage" name="extend_storage" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                        @error('extend_storage')
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                        @enderror
-                    </div>
+            <div class="flex justify-evenly">
+                <div class="flex items-center mb-4">
+                    <input id="laptop" type="radio" value="" name="asset_type" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="laptop" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Laptop</label>
+                </div>
+                <div class="flex items-center mb-4">
+                    <input id="vehicle" type="radio" value="" name="asset_type" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    <label for="vehicle" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Vehicle</label>
                 </div>
             </div>
-            <div>
-                <p class="text-center">Display</p>
-                <div class="flex gap-3">
-                    <div class="mb-6">
-                        <label for="vga" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">VGA</label>
-                        <input type="text" id="vga" name="vga" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                        @error('vga')
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                        @enderror
-                    </div>
-                    <div class="mb-6">
-                        <label for="monitor" class="block text-end mb-2 text-sm font-medium text-gray-900 dark:text-white">Monitor</label>
-                        <input type="text" id="monitor" name="monitor" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" autocomplete="off" autofocus required>
-                        @error('monitor')
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
-                        @enderror
-                    </div>
-                </div>
+            <div class="mb-6">
+                <label for="assets" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                <select id="assets" name="assets" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled>
+                    <option selected>Choose an Asset</option>
+                    <option value="US">United States</option>
+                    <option value="CA">Canada</option>
+                    <option value="FR">France</option>
+                    <option value="DE">Germany</option>
+                </select>
             </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
